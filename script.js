8dfaea3f0c8d0bc3b1899863f49827c3601d5371
Innocent <Innocentbuthelezi@icloud.com>
2021-01-14 15:32:50 -0800

    server-added

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const quoteContainer = document.getElementById('quote-container')
-const quoteText = document.getElementById('quote')
-const authorText = document.getElementById('author')
-const twitterBtn = document.getElementById('twitter')
-const qnewQuoteBtn = document.getElementById('new-quote')
+// const quoteContainer = document.getElementById('quote-container')
+// const quoteText = document.getElementById('quote')
+// const authorText = document.getElementById('author')
+// const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
 
 // Get Quote From API
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const proxyUrl = 'http://localhost:3001/api/proxy'
     try {
-        const response = await fetch(proxyUrl + apiUrl);
+        const response = await fetch(proxyUrl);
         const data = await response.json();
-        authorText.innerText = data.quoteAuthor;
-        quoteText.innerText = data.quoteText;
+        console.log(data);
+        // authorText.innerText = data.quoteAuthor;
+        // quoteText.innerText = data.quoteText;
     } catch (error) {
-        getQuote();
+        console.log('whoops, no quote', error);
     }
+    // getQuote();
 }
 
 // On Load
